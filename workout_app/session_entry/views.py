a0df39deda84b15b0e34a392d4fb0b289d73b070
jmparr63 <jmparr63@gmail.com>
2024-10-19 23:49:01 +0100

    Added basic cardio form

--- workout_app/session_entry/views.py
+++ workout_app/session_entry/views.py
@@ -1,8 +1,12 @@
 from django.http import HttpResponse
 from django.template import loader
 from django.shortcuts import render
+from django.template.loader import render_to_string
+
 
 from .models import Session
+from .forms import WorkoutForm, CardioExerciseForm
+
 
 def index(request):
     latest_session_list = [s.date.strftime('%a %d %b %Y, %I:%M%p') for s in Session.objects.order_by("-date")[:5]]
@@ -16,8 +20,29 @@ def summary(request, session_id):
     response = "You're looking at the results of session %s"
     return HttpResponse(response % session_id)
 
+
+
 def capture(request):
-    return render(request, "session_entry/capture.html")
+    if request.method == 'POST':
+        form = WorkoutForm(request.POST)
+        if form.is_valid():
+            # Process the form data
+            # You'll need to iterate through the formsets to access all the data
+            pass
+    else:
+        form = WorkoutForm()
+
+    return render(request, "session_entry/capture.html", {'form': form})
+
+def get_cardio_form(request):
+    form_index = request.GET.get('form_index')
+    cardio_form = CardioExerciseForm(prefix=f'{form_index}')
+    context = {'form': cardio_form}
+    html = render_to_string('session_entry/cardio_form.html', context)
+    return HttpResponse(html)
+
+
+
 
 def analyse(request):
     return render(request, "session_entry/analyse.html")
--- workout_app/workout_app/settings.py
+++ workout_app/workout_app/settings.py
@@ -32,6 +32,8 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'session_entry.apps.SessionEntryConfig',
+    'crispy_forms',
+    'crispy_bootstrap4',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -122,3 +124,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
